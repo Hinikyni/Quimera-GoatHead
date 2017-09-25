7e99ae180461a982d4c6adf2e4755e5d62639511
Branilson Luiz <branilson@gmail.com>
2017-09-24 23:43:55 -0300

    Creation cmdVel Publishers,  updates on motor control functions and added startup beeps

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,17 @@
 #include <Arduino.h>
 #include <ros.h>
 #include <rosserial_arduino/Adc.h>
+#include <std_msgs/Int16.h>
+#include <geometry_msgs/Twist.h>
 #include <main.hpp>
 
 ros::NodeHandle nh;
 
 rosserial_arduino::Adc adc_msg;
 ros::Publisher p("adc", &adc_msg);
-
-
-//#include <digitalWriteFast.h>   // library for high performance reads and writes by jrraines
-                                // see http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1267553811/0
-                                // and http://code.google.com/p/digitalwritefast/
-
+ros::Subscriber<std_msgs::Int16> subCmdLeft("cmd_left_wheel", moveLeftMotorCB);
+ros::Subscriber<std_msgs::Int16> subCmdRight("cmd_right_wheel", moveRightMotorCB);
+ros::Subscriber<geometry_msgs::Twist> subCmdVel("cmd_vel", cmdVelCB);
 
 void setup() 
 {
@@ -20,19 +19,24 @@ void setup()
   setupMotors();
   nh.initNode();
   nh.advertise(p);
+  nh.subscribe(subCmdRight);
+  nh.subscribe(subCmdLeft);
+  bipGen(800, 200, 333, 3); // Startup bips. It takes 1s.
 }
 
 void loop() 
 {
-  adc_msg.adc0 = averageAnalog(0); 
-  adc_msg.adc1 = averageAnalog(1);
-  adc_msg.adc2 = averageAnalog(2);
-  adc_msg.adc3 = averageAnalog(3);
-  adc_msg.adc4 = averageAnalog(4);
-  adc_msg.adc5 = averageAnalog(5);
-    
-  p.publish(&adc_msg);
-  
+  if (loop_time < millis())
+  {
+    adc_msg.adc0 = averageAnalog(0); 
+    adc_msg.adc1 = averageAnalog(1);
+    adc_msg.adc2 = averageAnalog(2);
+    adc_msg.adc3 = averageAnalog(3);
+    adc_msg.adc4 = averageAnalog(4);
+    adc_msg.adc5 = averageAnalog(5);
+    p.publish(&adc_msg);
+    loop_time = millis() + 1000;
+  }
   nh.spinOnce();
 }
 
@@ -71,44 +75,53 @@ void doRightEncoderB()
 void setupMotors()
 {
   pinMode(left_motor_enable_pin, OUTPUT);
-  digitalWrite(left_motor_enable_pin, HIGH); // Starting with motor enabled
   pinMode(left_motor_direction_pin, OUTPUT);
-  digitalWrite(left_motor_direction_pin, LOW);
   pinMode(left_motor_pwm_pin, OUTPUT);
-  digitalWrite(left_motor_pwm_pin, LOW);
   pinMode(right_motor_enable_pin, OUTPUT);
-  digitalWrite(right_motor_enable_pin, HIGH); // Starting with motor enabled
   pinMode(right_motor_direction_pin, OUTPUT);
-  digitalWrite(right_motor_direction_pin, LOW);
   pinMode(right_motor_pwm_pin, OUTPUT);
+  digitalWrite(left_motor_pwm_pin, LOW);
   digitalWrite(right_motor_pwm_pin, LOW);
+  motor_status = motorStatus (ON);
 }
 
-void moveLeftMotor(int left_pwm)
+int motorStatus()
 {
-  if (left_pwm>=0)
+  return motor_status;
+}
+
+int motorStatus(int status)
+{
+  digitalWrite(left_motor_enable_pin, status);
+  digitalWrite(right_motor_enable_pin, status);
+  return status;
+}
+
+void moveLeftMotorCB(const std_msgs::Int16& msg)
+{
+  if (msg.data >=0 )
   {
     digitalWrite(left_motor_direction_pin, HIGH);
-    analogWrite(left_motor_pwm_pin, left_pwm);
+    analogWrite(left_motor_pwm_pin, msg.data);
   }
   else
   {
-    digitalWrite(left_motor_direction_pin, HIGH);
-    analogWrite(left_motor_pwm_pin, abs(left_pwm));
+    digitalWrite(left_motor_direction_pin, LOW);
+    analogWrite(left_motor_pwm_pin, -msg.data);
   }
 }
 
-void moveRightMotor(int right_pwm)
+void moveRightMotorCB(const std_msgs::Int16& msg)
 {
-  if (right_pwm>=0)
+  if (msg.data >= 0)
   {
     digitalWrite(right_motor_direction_pin, HIGH);
-    analogWrite(right_motor_pwm_pin, right_pwm);
+    analogWrite(right_motor_pwm_pin, msg.data);
   }
   else
   {
-    digitalWrite(right_motor_direction_pin, HIGH);
-    analogWrite(right_motor_pwm_pin, abs(right_pwm));
+    digitalWrite(right_motor_direction_pin, LOW);
+    analogWrite(right_motor_pwm_pin, -msg.data);
   }
 }
 
@@ -116,4 +129,46 @@ int averageAnalog(int pin){
   int v=0;
   for(int i=0; i<4; i++) v+= analogRead(pin);
   return v/4;
+}
+
+void bipGen (unsigned int sound_freq, unsigned long time_on, unsigned long period, unsigned int n_cycles)
+{
+  for (unsigned int i = 1; i <= n_cycles; i++)
+    {
+      tone(buzzer_pin, sound_freq, time_on);
+      if (period > 0 && n_cycles > 0 && i < n_cycles)
+      {
+        delay(period);
+      }
+    }
+}
+
+void cmdVelCB( const geometry_msgs::Twist& twist)
+{
+  int gain = 4000;
+  float left_wheel_data = gain*(twist.linear.x - twist.angular.z*l_wheels);
+  float right_wheel_data = gain*(twist.linear.x + twist.angular.z*l_wheels);
+  
+  if(right_wheel_data >= 0)
+  {
+    digitalWrite(left_motor_direction_pin, HIGH);
+    analogWrite(left_motor_pwm_pin, abs (left_wheel_data));
+  }
+  else
+  {
+    digitalWrite(left_motor_direction_pin, LOW);
+    analogWrite(left_motor_pwm_pin, abs (left_wheel_data));
+  }
+
+  if(right_wheel_data >= 0)
+  {
+    digitalWrite(right_motor_direction_pin, HIGH);
+    analogWrite(right_motor_pwm_pin, abs (right_wheel_data));
+
+  }
+  else
+  {
+    digitalWrite(right_motor_direction_pin, LOW);
+    analogWrite(right_motor_pwm_pin, abs (right_wheel_data));
+  }
 }
--- src/main.hpp
+++ src/main.hpp
@@ -1,50 +1,64 @@
 #ifndef MAIN_HPP_
 #define MAIN_HPP_
 
+/*  Library for high performance reads and writes by jrraines
+*   see http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1267553811/0
+*   and http://code.google.com/p/digitalwritefast/
+*/
+//#include <digitalWriteFast.h>   
+
 // Outputs Pins definition
-const int left_motor_enable_pin = 5;
-const int left_motor_direction_pin = 6;
-const int left_motor_pwm_pin = 7;
-const int right_motor_enable_pin = 9;
-const int right_motor_direction_pin = 10;
-const int right_motor_pwm_pin = 11;
-const int buzzer_pin = 12;
+const unsigned int left_motor_enable_pin = 4;
+const unsigned int left_motor_direction_pin = 5;
+const unsigned int left_motor_pwm_pin = 6;
+const unsigned int right_motor_enable_pin = 9;
+const unsigned int right_motor_direction_pin = 10;
+const unsigned int right_motor_pwm_pin = 11;
+const unsigned int buzzer_pin = 12;
 
-// Digital Inputs definition
-const int left_encoder_a_pin = 2;
-const int left_encoder_b_pin = 3;
-const int right_encoder_a_pin = 20;
-const int right_encoder_b_pin = 21;
+/* Digital Inputs definition */
+const unsigned int left_encoder_a_pin = 2;
+const unsigned int left_encoder_b_pin = 3;
+const unsigned int right_encoder_a_pin = 20;
+const unsigned int right_encoder_b_pin = 21;
 
-// Analog Inputs definition
-const int left_front_corner_sensor_pin = 0;
-const int right_front_corner_sensor_pin = 1;
-const int left_rear_corner_sensor_pin  = 2;
-const int Right_rear_corner_sensor_pin = 3;
-const int battery_voltage_pin = 4;
-const int battery_current_pin = 5;
+/* Analog Inputs definition */
+const unsigned int left_front_corner_sensor_pin = 0;
+const unsigned int right_front_corner_sensor_pin = 1;
+const unsigned int left_rear_corner_sensor_pin  = 2;
+const unsigned int Right_rear_corner_sensor_pin = 3;
+const unsigned int battery_voltage_pin = 4;
+const unsigned int battery_current_pin = 5;
 
-//Interrupts definition
+/* Interrupts definition */
 const int left_encoder_a_interrupt = 3;
 const int left_encoder_b_interrupt = 2;
 const int right_encoder_a_interrupt = 0;
 const int right_encoder_b_interrupt = 1;
 
-//encoder variables
+/* Other variables */
 volatile long left_encoder_position = 0;
 volatile long right_encoder_position = 0;
 volatile bool left_encoder_past_b = false;
 volatile bool right_encoder_past_b = false;
+enum motorstatus {ON = 0, OFF};
+volatile int motor_status = OFF;
+volatile long loop_time = 0;
+const float l_wheels = 0.12; //distance between wheels
 
-// Function protitypes
+/* Function protitypes */
 void setupEncoders();
 void doLeftEncoderA();
 void doLeftEncoderB();
 void doRightEncoderA();
 void doRightEncoderB();
 void setupMotors();
-void moveLeftMotor(int left_pwm);
-void moveRightMotor(int right_pwm);
+void moveLeftMotorCB(const std_msgs::Int16& msg);
+void moveRightMotorCB(const std_msgs::Int16& msg);
 int averageAnalog(int pin);
+int motorStatus();
+int motorStatus(int status);
+void bipGen (unsigned int sound_freq, unsigned long time_on, unsigned long period, unsigned int n_cycles);
+void cmdVelCB( const geometry_msgs::Twist& twist);
 
 #endif  // MAIN_HPP_
